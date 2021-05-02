0e2ac2b6b0dcff8ff837c8f1add2f4523e3dc94a
dragos <dragos042@yahoo.com>
2021-05-02 14:59:39 +0300

    button link to plan page fix

--- js/script.js
+++ js/script.js
@@ -1,14 +1,8 @@
 const hamburgerBtn = document.getElementById('hamburger');
 const mobileMenu = document.querySelector('.modal-menu');
-const createPlanBtn = document.querySelector('.create-plan-btn');
+const createPlanBtn = document.querySelectorAll('.create-plan-btn');
 
 hamburgerBtn.addEventListener('click', () => {
   hamburgerBtn.classList.toggle('open');
   mobileMenu.classList.toggle('hidden');
 });
-
-if (createPlanBtn) {
-  createPlanBtn.addEventListener('click', () => {
-    window.open('https://coffeeroastersco.netlify.app/plan.html');
-  });
-}
